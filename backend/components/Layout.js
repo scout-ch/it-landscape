b46e49c5337cfc0a84f428706fa4fd557bd7daeb
simfeld <sml.imfeld@gmail.com>
2023-03-26 17:46:50 +0200

    Remove posts component

--- backend/components/Layout.js
+++ backend/components/Layout.js
@@ -17,10 +17,6 @@ export const Layout = (props) => {
         <Link href="/">
           <a>Home</a>
         </Link>
-        {' | '}
-        <Link href="/posts">
-          <a>Posts</a>
-        </Link>
       </header>
       <main>{props.children}</main>
     </div>
