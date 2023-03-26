e87d8ede5589f42ff8aa5dda590f05385db190dc
simfeld <sml.imfeld@gmail.com>
2023-03-26 17:27:45 +0200

    Remove blog post collection

--- backend/.tina/config.js
+++ backend/.tina/config.js
@@ -24,32 +24,6 @@ const schema = defineSchema({
         },
       },
     },
-    {
-      label: "Blog Posts",
-      name: "post",
-      path: "content/post",
-      fields: [
-        {
-          type: "string",
-          label: "Title",
-          name: "title",
-        },
-        {
-          type: "string",
-          label: "Blog Post Body",
-          name: "body",
-          isBody: true,
-          ui: {
-            component: "textarea",
-          },
-        },
-      ],
-      ui: {
-        router: ({ document }) => {
-          return `/posts/${document._sys.filename}`;
-        },
-      },
-    },
   ],
 });
 
