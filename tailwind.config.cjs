91bcebff80e339c47120682eebdeec841bad0c57
Diego Steiner <diego.steiner@u041.ch>
2023-03-28 15:56:12 +0200

    feat: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			'primary': "#521d3a",
+			'bg': '#f8f7f6'
+		},
+	},
+	plugins: [],
+}
