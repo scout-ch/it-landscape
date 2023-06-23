27e4e130315d999f7de093b685d31c4a1a8df40f
Diego Steiner <diego.steiner@u041.ch>
2023-06-23 15:12:25 +0000

    feat: add parties

--- src/services/data.ts
+++ src/services/data.ts
@@ -17,3 +17,16 @@ export async function getServices() {
 
 	return services;
 }
+
+export async function getParties() {
+  const yamlParties = await import.meta.glob("../../backend/content/party/*.yaml");
+	const parties = new Map<string, any>()
+
+  await Promise.all(Object.entries(yamlParties).map(async ([path, yamlParty]) => {
+		const partyData = await yamlParty()
+		const id = path.match(/\.\.\/\.\.\/backend\/content\/party\/(.+)\.yaml/)?.[1].toLowerCase()
+		parties.set(path.replace(prefix, ""), { id, ...partyData.default });
+  }))
+
+	return parties;
+}
