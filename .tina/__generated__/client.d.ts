fa5f3902c28cc2e20834c0ad398ae3d398fdcc31
Emily Wangler <emily@at-point.ch>
2024-04-07 12:03:33 +0200

    cleanup stuff and add styling

--- .tina/__generated__/client.d.ts
+++ .tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '524c9323c760147cbd9b3b7be2bff0c88ee26ac7', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'unknown', queries,  });
 export default client;
   
