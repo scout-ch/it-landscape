f3fbd5f9329e439f8c265afab3d12a1714dd443c
Diego Steiner <diego.steiner@u041.ch>
2025-04-04 09:02:34 +0000

    remove old baggage

--- .tina/__generated__/client.d.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'unknown', queries,  });
-export default client;
-  
