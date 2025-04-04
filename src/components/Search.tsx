6c551e8997fd1d85757f3a0b734062c991e5736c
James Levell <jimmy.levell@outlook.com>
2025-04-04 18:47:32 +0200

    Fix: improve design

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -42,7 +42,7 @@ export default function Search(props: Props) {
       <input
         type="text"
         placeholder="Search..."
-        className="border border-gray-300 rounded p-2 w-full"
+        className="border border-black-300 rounded p-2 w-full"
         value={search}
         onChange={(e) => {
           setSearch(e.target.value);
@@ -53,7 +53,7 @@ export default function Search(props: Props) {
         }}
       />
 
-      <div>
+      <div className="mt-4">
         <table cellPadding="7" className="w-full table-auto">
           <thead
             style={{
@@ -71,7 +71,7 @@ export default function Search(props: Props) {
           </thead>
           <tbody>
             {filteredList?.map((service) => (
-              <tr key={service.id} className="even:bg-gray-50 odd:bg-white">
+              <tr key={service.id} className="even:bg-gray-100">
                 <td>
                   <a className="text-brombeer hover:underscore" href={`/it-landscape/services/${service.data.name}`}>
                     {service.data.name}
@@ -84,17 +84,16 @@ export default function Search(props: Props) {
                 </td>
                 <td>
                   <span
-                    className={`${
-                      service.data.criticality === "low"
-                        ? "bg-green-200"
-                        : service.data.criticality === "medium"
-                          ? "bg-orange-200"
-                          : service.data.criticality === "high"
-                            ? "bg-yellow-200"
-                            : service.data.criticality === "critical"
-                              ? "bg-red-200"
-                              : ""
-                    }`}
+                    className={`${service.data.criticality === "low"
+                      ? "bg-green-200"
+                      : service.data.criticality === "medium"
+                        ? "bg-orange-200"
+                        : service.data.criticality === "high"
+                          ? "bg-yellow-200"
+                          : service.data.criticality === "critical"
+                            ? "bg-red-200"
+                            : ""
+                      }`}
                   >
                     {service.data.criticality}
                   </span>
